df8d27e493618d95d205da509dee1ad3f3cb7f40
Pepe Marquez <64071335+Pepe-Marquez@users.noreply.github.com>
2025-01-28 20:23:22 +0000

    Fix test file

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -1,5 +1,5 @@
 def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
-    from foobar.apps import app_entry_point
+    from nomad_luqy_plugin.apps import app_entry_point
 
     assert app_entry_point.app.label == 'NewApp'
